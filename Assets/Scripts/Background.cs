e907d8469ca8ab12765bececc0b583aefb612900
dhsong215 <dhsong215@gmail.com>
2024-03-30 21:06:14 +0900

    Moving Background

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //private 는 다른 함수나 클래스에서 접근 불가
+    //f를 붙이면 실수 의미
+    private float moveSpeed = 3f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector3는 유니티 자체 제공
+        //Time.deltaTime을 곱하는 이유는 서로 다른 프레임이 나와도 시간이 지남에 따라 일정하게 움직이게 하기 위해
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < -10) {
+            transform.position += new Vector3(0, 20f, 0);
+        }
     }
 }
